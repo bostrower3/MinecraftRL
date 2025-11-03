6d00137bad5d3b1c086f9190fbcc93a7cf13f4fe
Ben <bostrower3@gatech.edu>
2025-11-02 20:46:26 -0500

    Init

---
+++ Readme.md
@@ -0,0 +1,7 @@
+work based off the following:
+https://arxiv.org/pdf/2509.13347
+https://arxiv.org/pdf/2112.04907
+https://arxiv.org/pdf/2303.10571v2
+https://arxiv.org/pdf/2301.04104
+https://arxiv.org/pdf/2410.07484
+https://arxiv.org/pdf/2412.10410
---
+++ src/agent.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class Agent(nn.Module):
+    """
+    Complete CDDM model with VAE and Diffusion.
+    """
+    
+    def __init__(
+        self,
+        
+    ):
+        """
+        
+        """
+        super(Agent, self).__init__()
